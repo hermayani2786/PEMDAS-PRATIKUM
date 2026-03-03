ac4ce4dd5c27bfb6d34e0a15fb2f058e012aa478
hermayani2786 <herma.yani2786@gmail.com>
2026-03-03 21:22:46 +0700

    menambahkan panjang dan lebar

--- latihan1.cpp
+++ latihan1.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 //deklarasi global
 int p, l, luas;
+
+// implementasi Prosedur dan Fungsi
+void input() {
+    cout << "masukkan panjang = ";
+    cin >> p;
+    cout << "masukkan lebar = ";
+    cin >> l;
+}
