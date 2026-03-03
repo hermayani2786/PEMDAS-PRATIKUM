eb80de21972757be988485bee7b0616f47c9150a
hermayani2786 <herma.yani2786@gmail.com>
2026-03-03 21:44:38 +0700

    memfinalisasi kode

--- latihan2.cpp
+++ latihan2.cpp
@@ -25,3 +25,9 @@ void output(){
     cout << "hasilnya =" << luasPersegiP(p, l) << endl;
     cout << "terima kasih" << endl;
 }
+
+int main() { //mulai
+    input();
+    output();
+    cout << "hasil penjumlahan= " << jumlah(15.1, 24.9, 67);
+} //selesai
